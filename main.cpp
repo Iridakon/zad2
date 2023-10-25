3d314bfa84773f4a786ae8647aaf6fd7ae1719e0
Iridakon <71597136+Iridakon@users.noreply.github.com>
2023-10-25 19:20:43 +0700

    +Гаусс по 2 и по 3 узлам

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "windows.h"
 #include "math.h"
+
 double f(double x){
     return ((x*sin(x))*(x*sin(x)));
 }
@@ -56,6 +57,36 @@ double Simp(double left, double right, int N)//метод Симnсона
     }
     return sum;
 }
+double Gauss2(double left, double right, int N)//метод Симnсона
+{
+    double sum=0;
+    double shag=(right-left)/N;
+    double pos=left;
+    double kor,ab;
+    for (int i = 0; i < N; ++i) {
+        kor=shag/(2* sqrt(3));
+        ab=pos+shag/2;
+        sum+=(f(ab+kor)+f(ab-kor))*shag/2;
+        pos+=shag;
+        //printf("%f %f\n",pos,sum);
+    }
+    return sum;
+}
+double Gauss3(double left, double right, int N)//метод Симnсона
+{
+    double sum=0;
+    double shag=(right-left)/N;
+    double pos=left;
+    double kor,ab;
+    for (int i = 0; i < N; ++i) {
+        kor=shag/2* sqrt(0.6);
+        ab=pos+shag/2;
+        sum+=((f(ab-kor)+f(ab+kor))*((float)5/9.0)+f(ab)*((float)8/9))*shag/2;
+        pos+=shag;
+        //printf("%f %f\n",f(ab+kor),f(ab+kor)*((float)5/9));
+    }
+    return sum;
+}
 double Better(double In,double In2,int p)
 {
     double alpha=1/(1-pow(2,p));
@@ -71,6 +102,7 @@ int main() {
 
     printf("Кол-во узлов   \t%i\t\t%i \t\tИзменение \tУлучш\n",N,N*2);
     double y1,y2;
+    /*
     y1=Ans(LPramoug(a,b,N),ans);
     y2=Ans(LPramoug(a,b,2*N),ans);
     printf("Левые пр        %8.12f   %8.12f  %5.2f   %8.12f\n",y1,y2,y1/y2, Better(y1,y2,1));
@@ -83,6 +115,13 @@ int main() {
     y1=Ans(Simp(a,b,N),ans);
     y2=Ans(Simp(a,b,N*2),ans);
     printf("Сипсон          %8.12f   %8.12f  %5.2f   %8.12f\n",y1,y2,y1/y2,Better(y1,y2,4));
+     */
+    y1=Ans(Gauss2(a,b,N),ans);
+    y2=Ans(Gauss2(a,b,N*2),ans);
+    printf("Гаусс по 2      %8.12f   %8.12f  %5.2f   %8.12f\n",y1,y2,y1/y2,Better(y1,y2,4));
+    y1=Ans(Gauss3(a,b,N),ans);
+    y2=Ans(Gauss3(a,b,N*2),ans);
+    printf("Гаусс по 3      %8.12f   %8.12f  %5.2f   %8.12f\n",y1,y2,y1/y2,Better(y1,y2,1));
 
     return 0;
 }
